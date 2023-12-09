793a5ea3cb6918d8f3f03fed4414397b9276f90e
“vukas86” <vukasbit@gmail.com>
2023-12-09 16:12:26 +0100

    edit nav component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,23 @@
-import React from "react";
+import hamburgerIcon from "../../assets/icons/burger/hamburger.png";
+import logo from "../../assets/logo/snappvista-removebg-preview.png";
+
+import styles from "./Navbar.module.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className={styles.navGroup}>
+        <div>
+          <button>
+            <img src={hamburgerIcon} alt="ham-icon"></img>
+          </button>
+        </div>
+        <img src={logo} alt="logo" />
+      </div>
+      <div>Search</div>
+      <div>Toggle</div>
+    </nav>
+  );
 }
 
 export default Navbar;
