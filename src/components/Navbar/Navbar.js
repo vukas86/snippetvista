fce4454a02b241355ba018d735bc2a08e8e3479f
“vukas86” <vukasbit@gmail.com>
2023-12-09 18:02:42 +0100

    additional edits of navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import hamburgerIcon from "../../assets/icons/burger/hamburger.png";
-import logo from "../../assets/logo/snappvista-removebg-preview.png";
+import logo from "../../assets/logo/snilogo-removebg-preview.png";
 
 import styles from "./Navbar.module.css";
 
@@ -8,11 +8,11 @@ function Navbar() {
     <nav>
       <div className={styles.navGroup}>
         <div>
-          <button>
+          <button className={styles.menuBtn}>
             <img src={hamburgerIcon} alt="ham-icon"></img>
           </button>
         </div>
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className={styles.logoImg} />
       </div>
       <div>Search</div>
       <div>Toggle</div>
