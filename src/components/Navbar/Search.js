4b06becad50d61ce26fcd77933ff2efca44198b6
“vukas86” <vukasbit@gmail.com>
2023-12-10 22:38:04 +0100

    edit search component

--- src/components/Navbar/Search.js
+++ src/components/Navbar/Search.js
@@ -1,11 +1,40 @@
-import searchIcon from "../../assets/icons/search/search-outline.png";
+// import searchIcon from "../../assets/icons/search/search-outline.png";
+import { useState, useEffect } from "react";
 
 import styles from "./Search.module.css";
 
 function Search() {
-  const searchEvent = (e) => {
-    e.preventDefault();
+  const data = [
+    {
+      url: "",
+      tags: ["react", "blog"],
+      title: "How to create a react search bar",
+    },
+    {
+      url: "",
+      tags: ["node", "express"],
+      title: "How to mock api data in Node",
+    },
+  ];
+
+  const [state, setstate] = useState({
+    query: "",
+    filteredData: [],
+  });
+
+  const searchHandler = (e) => {
+    // setQuery(e.target.value);
+
+    const results = data.filter((item) => {
+      if (e.target.value === "") return data;
+      return item.title.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setstate({
+      query: e.target.value,
+      filteredData: results,
+    });
   };
+
   return (
     <div className={styles.searchContainer}>
       <form>
@@ -13,10 +42,12 @@ function Search() {
           type="search"
           className={styles.searchBox}
           placeholder="Search"
+          value={state.query}
+          onChange={searchHandler}
         />
-        <button onClick={searchEvent} className={styles.searchBtn}>
+        {/* <button onClick={searchEvent} className={styles.searchBtn}>
           <img src={searchIcon} alt="search-icon" />
-        </button>
+        </button> */}
       </form>
     </div>
   );
