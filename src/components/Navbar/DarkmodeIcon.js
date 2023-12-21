4ff7cdfd97458c57f050d483de0fb0d20a93e7e1
“vukas86” <vukasbit@gmail.com>
2023-12-21 18:54:25 +0100

    modify toggle btn

---
+++ src/components/Navbar/DarkmodeIcon.js
@@ -0,0 +1,7 @@
+import darkModeIcon from "../../assets/icons/sun/sun-24.png";
+
+function DarkmodeIcon() {
+  return <img src={darkModeIcon} alt="Dark Mode" className="DarkModeIcon" />;
+}
+
+export default DarkmodeIcon;
--- src/components/Navbar/Toggle.js
+++ src/components/Navbar/Toggle.js
@@ -1,19 +1,35 @@
-import { useState } from "react";
-import styles from "./Toggle.module.css";
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 import Toggle from "react-toggle";
+import DarkmodeIcon from "./DarkmodeIcon";
+
+import "react-toggle/style.css";
+import styles from "./Toggle.module.css";
 
 function DarkModeToggle() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // const toggleDarkMode = () => {
-  //   setIsDarkMode((prevMode) => !prevMode);
-  // };
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: "(prefers-color-scheme:dark)",
+    },
+    undefined,
+    (isSystemDark) => setIsDarkMode(isSystemDark)
+  );
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDarkMode]);
 
   return (
     <Toggle
       checked={isDarkMode}
       onChange={({ target }) => setIsDarkMode(target.checked)}
-      icons={{ checked: "🌙", unchecked: "🔆" }}
+      icons={{ checked: null, unchecked: null }}
       aria-label="Dark mode toggle"
     />
   );
