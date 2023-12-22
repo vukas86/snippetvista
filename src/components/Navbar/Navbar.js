aac0d13c7c2e22b7cc3b7ffa2df13fec9665e0f4
“vukas86” <vukasbit@gmail.com>
2023-12-22 19:20:45 +0100

    adjusting nav bar to dark mode

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,27 @@
 import Search from "./Search";
+import { useDarkMode } from "../../store/DarkModeContext";
 
-import hamburgerIcon from "../../assets/icons/burger/hamburger.png";
-import logo from "../../assets/logo/snilogo-removebg-preview.png";
+import hamburgerIconBlack from "../../assets/icons/burger/ham-black.svg";
+import hamburgerIconWhite from "../../assets/icons/burger/ham-white.svg";
+import logoBlack from "../../assets/logo/snilogo-removebg-preview.png";
+import logoNoBg from "../../assets/logo/sv_logo-removebg-preview.png";
 
 import styles from "./Navbar.module.css";
 import DarkModeToggle from "./Toggle";
 
 function Navbar() {
+  const { isDarkMode, setIsDarkMode } = useDarkMode();
+  const logo = isDarkMode ? logoNoBg : logoBlack;
+  const hamburgerIcon = isDarkMode ? hamburgerIconWhite : hamburgerIconBlack;
+
   return (
     <nav>
       <div className={styles.navGroup}>
-        <div>
-          <button className={styles.menuBtn}>
-            <img src={hamburgerIcon} alt="ham-icon"></img>
-          </button>
-        </div>
+        <img
+          src={hamburgerIcon}
+          alt="ham-icon"
+          className={styles.hamIcon}
+        ></img>
         <img src={logo} alt="logo" className={styles.logoImg} />
       </div>
       <Search />
